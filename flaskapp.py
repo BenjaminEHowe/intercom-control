e7772b4aa3eb29484bf2af4d2ae932072b09b405
Benjamin Howe <ben@beh.uk>
2024-09-21 21:55:26 +0100

    Added basic login page

--- flaskapp.py
+++ flaskapp.py
@@ -5,13 +5,26 @@ import git
 app = flask.Flask(__name__)
 
 
+def get_git_hash():
+  repo = git.Repo()
+  return repo.head.object.hexsha
+
+
 @app.route('/', methods=['GET'])
 def index():
+  return flask.render_template(
+    "about.html",
+    hash = get_git_hash()
+  )
+
+
+@app.route('/login', methods=['GET'])
+def login():
   repo = git.Repo()
   hash = repo.head.object.hexsha
   return flask.render_template(
-    "about.html",
-    hash=hash
+    "login.html",
+    hash = get_git_hash()
   )
 
 
