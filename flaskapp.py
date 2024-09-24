aabcc6aa9319aacf4412f64a86aac1bba844ff98
Benjamin Howe <ben@beh.uk>
2024-09-24 16:42:30 +0100

    Hash passwords with `Argon2id`

--- flaskapp.py
+++ flaskapp.py
@@ -1,3 +1,4 @@
+import argon2
 import flask
 import flask_login
 import flask_wtf
@@ -9,6 +10,8 @@ import wtforms.validators
 
 app = flask.Flask(__name__)
 app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
+argon2_memory_cost = int(os.environ.get("ARGON2_MEMORY_COST") or "65536")
+argon2_time_cost = int(os.environ.get("ARGON2_TIME_COST") or "3")
 
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
@@ -19,8 +22,22 @@ class LoginForm(flask_wtf.FlaskForm):
   password = wtforms.PasswordField("Password", validators=[wtforms.validators.DataRequired()])
 
   def validate_username(self, field):
-    if self.password.data != "ShouldBeHashed":
-      raise wtforms.validators.ValidationError("Invalid username and / or password")
+    # note: this function actually validates the username _and_ password
+    generic_error = "Invalid username and / or password"
+    users = {
+      "user": "$argon2id$v=19$m=65536,t=3,p=4$+vAURrm9UMTsji9V23LXZw$T9A8ivcrWDHXd2/iAGalnwDhbFTBVl2v3u3ywtxZ6FY", # ShouldBeHashed
+    }
+    if self.username.data not in users.keys():
+      raise wtforms.validators.ValidationError(generic_error)
+    password_hasher = argon2.PasswordHasher(
+      memory_cost=argon2_memory_cost,
+      time_cost=argon2_time_cost,
+    )
+    try:
+      password_hasher.verify(users[self.username.data], self.password.data)
+      # TODO: check if password needs rehash, see https://argon2-cffi.readthedocs.io/en/23.1.0/howto.html
+    except argon2.exceptions.VerifyMismatchError:
+      raise wtforms.validators.ValidationError(generic_error)
 
 
 class LogoutForm(flask_wtf.FlaskForm):
